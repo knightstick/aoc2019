bfbb6f141e4c7054d42143716aac8e3cade900a3
Chris Jewell <chrisjohnjewell@gmail.com>
2019-12-21 16:52:13 +1100

    Rename some parts of the Executor and document

--- intcode/executor.go
+++ intcode/executor.go
@@ -1,26 +1,35 @@
 package intcode
 
+// Opcode is a translation from integer to action the program wants to take
 type Opcode int
 
 const (
-	ADD      = 1
+	// ADD takes two input positions and puts the sum in the output
+	ADD = 1
+	// MULTIPLY takes two input positions and puts the product in the output
 	MULTIPLY = 2
-	HALT     = 99
+	// HALT tells the program to stop
+	HALT = 99
 )
 
+// An Executor takes a program to put into memory, and then can execute the
+// instructionns
 type Executor struct {
-	program  *Program
-	position int
+	memory             *Program
+	instructionPointer int
 }
 
+// NewExecutor builds a new instance of Executor with the specified program
+// as the initial state of the memory
 func NewExecutor(program *Program) *Executor {
 	executor := new(Executor)
-	executor.program = program
-	executor.position = 0
+	executor.memory = program
+	executor.instructionPointer = 0
 
 	return executor
 }
 
+// Execute runs the program in memory until it halts
 func (e *Executor) Execute() {
 	if e.NextOpcode() == HALT {
 		// println("halting")
@@ -40,36 +49,42 @@ func (e *Executor) Execute() {
 	}
 }
 
+// Add uses the instruction to take values and add them together, then places
+// them in the output
 func (e *Executor) Add() {
-	input0position := e.program.ValueAt(e.position + 1)
-	input1position := e.program.ValueAt(e.position + 2)
-	outputPosition := e.program.ValueAt(e.position + 3)
+	input0position := e.memory.ValueAt(e.instructionPointer + 1)
+	input1position := e.memory.ValueAt(e.instructionPointer + 2)
+	outputPosition := e.memory.ValueAt(e.instructionPointer + 3)
 
-	input0value := e.program.ValueAt(input0position)
-	input1value := e.program.ValueAt(input1position)
+	input0value := e.memory.ValueAt(input0position)
+	input1value := e.memory.ValueAt(input1position)
 
-	e.program.ReplaceAt(outputPosition, input0value+input1value)
+	e.memory.ReplaceAt(outputPosition, input0value+input1value)
 
 	e.Step()
 }
 
+// Multiply uses the instruction to take values and multiply them together,
+// then places the product in the output
 func (e *Executor) Multiply() {
-	input0position := e.program.ValueAt(e.position + 1)
-	input1position := e.program.ValueAt(e.position + 2)
-	outputPosition := e.program.ValueAt(e.position + 3)
+	input0position := e.memory.ValueAt(e.instructionPointer + 1)
+	input1position := e.memory.ValueAt(e.instructionPointer + 2)
+	outputPosition := e.memory.ValueAt(e.instructionPointer + 3)
 
-	input0value := e.program.ValueAt(input0position)
-	input1value := e.program.ValueAt(input1position)
+	input0value := e.memory.ValueAt(input0position)
+	input1value := e.memory.ValueAt(input1position)
 
-	e.program.ReplaceAt(outputPosition, input0value*input1value)
+	e.memory.ReplaceAt(outputPosition, input0value*input1value)
 
 	e.Step()
 }
 
+// NextOpcode finds the Opcode at the start of the next instuction to execute
 func (e *Executor) NextOpcode() Opcode {
-	return Opcode(e.program.ValueAt(e.position))
+	return Opcode(e.memory.ValueAt(e.instructionPointer))
 }
 
+// Step moves the instruction pointer forward
 func (e *Executor) Step() {
-	e.position += 4
+	e.instructionPointer += 4
 }
